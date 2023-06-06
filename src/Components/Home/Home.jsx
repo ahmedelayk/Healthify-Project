0fe75bdcb5e5eb0cb86f328c4755bce5a40eb297
Ahmed Elayek <50213964+ahmedelayk@users.noreply.github.com>
2023-06-06 12:20:53 +0300

    creat nutrition and meditation section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Section1 from './Sections/section1/Section1'
 import Section2 from './Sections/section2/Section2'
+import Section3 from './Sections/section3/Section3'
+import Section4 from './Sections/section4/section4'
 
 function Home() {
   return (
-    <div className='container'>
+    <div className=''>
       <Section1/>
       <Section2/>
+      <Section3/>
+      <Section4/>
     </div>
   )
 }
--- src/Components/Home/Sections/section1/Section1.jsx
+++ src/Components/Home/Sections/section1/Section1.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import Macro from "../../../assets/images/macro.png";
 import { NavLink } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Container, Button, Row } from "react-bootstrap";
 import "./section1.css";
 const Section1 = () => {
   return (
-    <section id="landing" className="row col-12 section-padding-bottom">
-      <div className="col-lg-6 col-md-12 col-12  p-5 part1">
-        <h2 className="macro-text">
-          Unlock Your Potential. Empowering Health and Wellness for All!
-        </h2>
-        <NavLink to="/login" className="">
-          <Button className="go-btn">
-            Start your journey
-          </Button>
-        </NavLink>
-      </div>
-      <img src={Macro} className="col-lg-6 col-md-6 col-12 macro-image" alt="macro"/>
-    </section>
+    <Container id="landing" className="section-padding-bottom">
+      <Row>
+        <div className="col-lg-6 col-12  p-5 part1 d-flex flex-column justify-content-center">
+          <h2 className="macro-text">
+            Unlock Your Potential. Empowering Health and Wellness for All!
+          </h2>
+          <NavLink to="/login">
+            <Button className="go-btn">
+              Start your journey
+            </Button>
+          </NavLink>
+        </div>
+        <div className="col-md-6 col-12  d-flex align-items-center mt-1">
+          <img src={Macro} className="w-100 macro-image" alt="macro"/>
+        </div>
+      </Row>
+    </Container>
   );
 };
 
