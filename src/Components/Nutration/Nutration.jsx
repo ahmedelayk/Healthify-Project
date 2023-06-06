c6892e1a6b711c29790e4b707be23be0e3a5d0e4
AhmedAbouBakkr <aboubakrahmed8@gmail.com>
2023-06-06 08:45:12 +0300

    test

--- src/Components/Nutration/Nutration.jsx
+++ src/Components/Nutration/Nutration.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Nutration = () => {
-  return (
-    <div>Nutration</div>
-  )
-}
+  return <div></div>;
+};
 
-export default Nutration
+export default Nutration;
