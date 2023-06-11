c24928742eea4550325242398f729ad85fd15c99
AhmedAbouBakkr <aboubakrahmed8@gmail.com>
2023-06-11 16:40:48 +0300

    Nutration

--- src/Components/Nutration/Nutration.jsx
+++ src/Components/Nutration/Nutration.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { BMICalculator } from "./BMICalculator";
+import { BMIResult } from "./BMIResult";
+import { CaloriesTrack } from "./CaloriesTrack";
+import { ProgressTrack } from "./ProgressTrack";
+import { WaterTrack } from "./WaterTrack";
 
 const Nutration = () => {
   return (
-    <div>Nutration</div>
-  )
-}
+    <Container>
+      <Row className="justify-content-between">
+        <BMICalculator />
+        {/* <Col lg={1} className="padding"></Col> */}
+        <BMIResult />
+        <CaloriesTrack />
+        {/* <Col lg={1} className="padding"></Col> */}
+        <WaterTrack />
+        {/* <Col lg={1} className="padding"></Col> */}
+        <ProgressTrack />
+      </Row>
+    </Container>
+  );
+};
 
-export default Nutration
+export default Nutration;
