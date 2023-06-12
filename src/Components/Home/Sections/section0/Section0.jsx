e19bcd7293a78ac8b680f8ffbd07ce4a986859ef
Ahmed Elayek <50213964+ahmedelayk@users.noreply.github.com>
2023-06-12 15:32:07 +0300

    some edits

---
+++ src/Components/Home/Sections/section0/Section0.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Section0() {
+  return (
+    <>
+        <div>
+            <video width="320" height="240" controls>
+                <source src="../../../assets/videos/fitness.mp4" type="video/mp4" />
+
+            </video>
+            {/* <video width="320" height="240" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+            </video> */}
+        </div>
+    </>
+  )
+}
