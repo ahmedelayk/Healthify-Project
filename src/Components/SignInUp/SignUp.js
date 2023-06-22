1a27edc19950c564255c0ac735a147959c85e775
Sameh Hammad <eng.sameh1995@gmail.com>
2023-06-22 18:07:26 +0300

    adding google and facebook auth

--- src/Components/SignInUp/SignUp.js
+++ src/Components/SignInUp/SignUp.js
@@ -2,6 +2,8 @@
 import { Link, useNavigate } from "react-router-dom";
 // icons
 import { FaLock, FaUser, FaPhoneAlt, FaUsers } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 // context
 import { useAuth } from "../../Context/AuthContext";
@@ -11,195 +13,228 @@ import signUpImg from "../assets/images/signUpImg.webp";
 import "./signInUp.css";
 // react hook form
 import { useForm } from "react-hook-form";
+import { Col, Row } from "react-bootstrap";
+import { useEffect } from "react";
 
 function SignUp() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        trigger,
-    } = useForm()
-    const { signup } = useAuth();
-    const navigate = useNavigate()
-    const onSubmit = async (data) => {
-        console.log(data);
-        const { firstName, lastName, mail, phoneNumber, gender, password } = data;
-        await signup(firstName, lastName, phoneNumber, gender, mail, password);
-        navigate('/settings');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm();
+  const { signup,currentUser } = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = async (data) => {
+    console.log(data);
+    const { firstName, lastName, mail, phoneNumber, gender, password } = data;
+    await signup(firstName, lastName, phoneNumber, gender, mail, password);
+  };
+  useEffect(() => {
+    if (currentUser != null) {
+      console.log(currentUser);
+
+      navigate("/settings");
     }
-
-    return (
-        <div className="sec_container_signup d-flex justify-content-center align-items-center">
-            <div className="signInUp_container p-5 login template d-flex gap-5 justify-content-center align-items-center bg-white rounded">
-                <div
-                    className="form_container rounded bg-white"
-                    data-aos="fade-up"
-                    data-aos-anchor-placement="top-bottom"
-                >
-                    <form onSubmit={ handleSubmit(onSubmit) }>
-                        <h2 className="text-start mb-4">Register</h2>
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <input
-                                type="text"
-                                placeholder="First name"
-                                className="form-control form-control-login"
-                                { ...register("firstName", {
-                                    required: "required",
-                                    pattern: {
-                                        value: /^[a-zA-Z]{3,}$/ig,
-                                        message: "name must be at least 3 char.",
-                                    }
-                                }
-                                ) }
-                                onKeyUp={ () => {
-                                    trigger("firstName");
-                                } }
-                            />
-                            <FaUser className="icon-login" />
-                        </div>
-                        { errors.firstName && <p className="text-danger">{ errors.firstName?.message }</p> }
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <input
-                                type="text"
-                                placeholder="Last name"
-                                className="form-control form-control-login"
-                                { ...register("lastName", {
-                                    required: "required",
-                                    pattern: {
-                                        value: /^[a-zA-Z]{3,}$/ig,
-                                        message: "name must be at least 3 char.",
-                                    }
-                                }
-                                ) }
-                                onKeyUp={ () => {
-                                    trigger("lastName");
-                                } }
-                            />
-                            <FaUser className="icon-login" />
-                        </div>
-                        { errors.lastName && <p className="text-danger">{ errors.lastName?.message }</p> }
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <input
-                                type="email"
-                                placeholder="E-mail"
-                                className="form-control form-control-login"
-                                { ...register("mail", {
-                                    required: "required",
-                                    pattern: {
-                                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ig,
-                                        message: "invalid email",
-                                    }
-                                }
-                                ) }
-                                onKeyUp={ () => {
-                                    trigger("mail");
-                                } }
-                            />
-                            <MdEmail className="icon-login" />
-                        </div>
-                        { errors.mail && <p className="text-danger">{ errors.mail?.message }</p> }
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <input
-                                type="tel"
-                                placeholder="Phone number"
-                                className="form-control form-control-login"
-                                { ...register("phoneNumber", {
-                                    required: "required",
-                                    pattern: {
-                                        value: /^(010|011|012|015)\d{8}$/ig,
-                                        message: "invalid phone number",
-                                    }
-                                }
-                                ) }
-                                onKeyUp={ () => {
-                                    trigger("phoneNumber");
-                                } }
-                            />
-                            <FaPhoneAlt className="icon-login" />
-                        </div>
-                        { errors.phoneNumber && <p className="text-danger">{ errors.phoneNumber?.message }</p> }
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <select className="form-select form-select-login" id="gender"
-                                { ...register("gender") }
-
-                            >
-                                <option disabled defaultValue hidden>
-                                    Gender
-                                </option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                            <FaUsers className="icon-login" />
-                        </div>
-                        <div
-                            className="mb-2 inputDiv"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                className="form-control form-control-login"
-                                { ...register("password", {
-                                    required: "required",
-                                    pattern: {
-                                        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/ig,
-                                        message: "at least 8 chars and numbers",
-                                    }
-                                }
-                                ) }
-                                onKeyUp={ () => {
-                                    trigger("password");
-                                } }
-                            />
-                            <FaLock className="icon-login" />
-                        </div>
-                        { errors.password && <p className="text-danger">{ errors.password?.message }</p> }
-                        <div className="d-grid mt-4">
-                            <button className="btn btn-primary">Sign Up</button>
-                        </div>
-                        <p
-                            className="mt-2 paragraph-size"
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="top-bottom"
-                        >
-                            Already have an account?
-                            <Link to="/login" className="text-decoration-none">
-                                Login Now!
-                            </Link>
-                        </p>
-                    </form>
-                </div>
-                <div
-                    className="img_container"
-                    data-aos="flip-up"
-                    data-aos-anchor-placement="top-bottom"
-                >
-                    <img src={ signUpImg } alt="signUpImg" className="login-img" loading="lazy" />
-                </div>
+  });
+  return (
+    <div className="sec_container_signup d-flex justify-content-center align-items-center">
+      <div className="signInUp_container p-5 login template d-flex gap-5 justify-content-center align-items-center bg-white rounded">
+        <div
+          className="form_container rounded bg-white"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="text-start mb-4">Register</h2>
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <input
+                type="text"
+                placeholder="First name"
+                className="form-control form-control-login"
+                {...register("firstName", {
+                  required: "required",
+                  pattern: {
+                    value: /^[a-zA-Z]{3,}$/gi,
+                    message: "name must be at least 3 char.",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("firstName");
+                }}
+              />
+              <FaUser className="icon-login" />
             </div>
+            {errors.firstName && (
+              <p className="text-danger">{errors.firstName?.message}</p>
+            )}
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <input
+                type="text"
+                placeholder="Last name"
+                className="form-control form-control-login"
+                {...register("lastName", {
+                  required: "required",
+                  pattern: {
+                    value: /^[a-zA-Z]{3,}$/gi,
+                    message: "name must be at least 3 char.",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("lastName");
+                }}
+              />
+              <FaUser className="icon-login" />
+            </div>
+            {errors.lastName && (
+              <p className="text-danger">{errors.lastName?.message}</p>
+            )}
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <input
+                type="email"
+                placeholder="E-mail"
+                className="form-control form-control-login"
+                {...register("mail", {
+                  required: "required",
+                  pattern: {
+                    value:
+                      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/gi,
+                    message: "invalid email",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("mail");
+                }}
+              />
+              <MdEmail className="icon-login" />
+            </div>
+            {errors.mail && (
+              <p className="text-danger">{errors.mail?.message}</p>
+            )}
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <input
+                type="tel"
+                placeholder="Phone number"
+                className="form-control form-control-login"
+                {...register("phoneNumber", {
+                  required: "required",
+                  pattern: {
+                    value: /^(010|011|012|015)\d{8}$/gi,
+                    message: "invalid phone number",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("phoneNumber");
+                }}
+              />
+              <FaPhoneAlt className="icon-login" />
+            </div>
+            {errors.phoneNumber && (
+              <p className="text-danger">{errors.phoneNumber?.message}</p>
+            )}
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <select
+                className="form-select form-select-login"
+                id="gender"
+                {...register("gender")}
+              >
+                <option disabled defaultValue hidden>
+                  Gender
+                </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+              <FaUsers className="icon-login" />
+            </div>
+            <div
+              className="mb-2 inputDiv"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              <input
+                type="password"
+                placeholder="Password"
+                className="form-control form-control-login"
+                {...register("password", {
+                  required: "required",
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/gi,
+                    message: "at least 8 chars and numbers",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("password");
+                }}
+              />
+              <FaLock className="icon-login" />
+            </div>
+            {errors.password && (
+              <p className="text-danger">{errors.password?.message}</p>
+            )}
+            <div className="d-grid mt-4">
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+            <p
+              className="mt-2 paragraph-size"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+            >
+              Already have an account?
+              <Link to="/login" className="text-decoration-none">
+                Login Now!
+              </Link>
+            </p>
+
+            <Row className="text-center">
+              <Col xs={12}>
+                <p className="mt-2 paragraph-size d-inline ">
+                  Try To Sign Up With &nbsp;
+                </p>
+                <FcGoogle
+                  className="social-icons"
+                  onClick={() => console.log("hhh")}
+                />
+                <p className="mt-2 paragraph-size d-inline ">Or</p>
+                <BsFacebook className="social-icons" />
+              </Col>
+            </Row>
+          </form>
         </div>
-    );
+        <div
+          className="img_container"
+          data-aos="flip-up"
+          data-aos-anchor-placement="top-bottom"
+        >
+          <img
+            src={signUpImg}
+            alt="signUpImg"
+            className="login-img"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -5,28 +5,32 @@ import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { auth, db } from "../firebase";
+import { auth, db, gProvider, fProvider } from "../firebase";
 import { collection, getDocs, getDoc, doc, setDoc } from "firebase/firestore";
 import { storage } from "../firebase";
-import { ref, getDownloadURL } from "firebase/storage"
+import { ref, getDownloadURL } from "firebase/storage";
+import { useNutrition } from "./NutritionContext";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const { result } = useNutrition()
+  console.log(result);
   const [users, setUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
   const [currentUserData, setCurrentUserData] = useState({});
-  const usersRef = collection(db, 'users');
-  const [userImage, setUserImage] = useState("")
+  const usersRef = collection(db, "users");
+  const [userImage, setUserImage] = useState("");
 
   // get all users from fire store
   const fetchAllUsers = async () => {
     const data = await getDocs(usersRef);
     const users = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     return users;
-  }
+  };
 
   // fetch user data
   const fetchUserData = async (userId) => {
@@ -37,18 +41,18 @@ const AuthProvider = ({ children }) => {
         const userData = userDoc.data();
         return userData;
       } else {
-        console.log('error finding user');
+        console.log("error finding user");
       }
-    } catch (error) { console.log(error.message) }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
-
   const getImageFromFirebase = (imageUrl) => {
-    getDownloadURL(ref(storage, imageUrl))
-      .then((url) => {
-        setUserImage(url);
-      })
-  }
+    getDownloadURL(ref(storage, imageUrl)).then((url) => {
+      setUserImage(url);
+    });
+  };
   const getImage = () => {
     const userImageRef = ref(storage, `usersImages/${currentUserData?.userId}`);
     if (userImageRef) {
@@ -57,17 +61,17 @@ const AuthProvider = ({ children }) => {
           setUserImage(url);
         })
         .catch((error) => {
-          if (currentUserData?.gender === 'female') {
+          if (currentUserData?.gender === "female") {
             getImageFromFirebase(`usersImages/avatar-female.webp`);
-          } else if(currentUserData?.gender === 'male') {
+          } else if (currentUserData?.gender === "male") {
             getImageFromFirebase(`usersImages/avatar-male.webp`);
           }
         });
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('context work');
+    console.log("context work");
 
     fetchAllUsers()
       .then((users) => setUsers(users))
@@ -83,7 +87,7 @@ const AuthProvider = ({ children }) => {
         setCurrentUser(null);
         setCurrentUserData(null);
       }
-    })
+    });
 
     getImage();
     // Clean up the subscription when component unmounts
@@ -92,22 +96,28 @@ const AuthProvider = ({ children }) => {
 
   // Authentication Operations (signup, login, logout)
   // Signup function
-  const signup = (firstName, lastName, phoneNumber, gender, email, password) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(
-        async (userCredential) => {
-          const user = userCredential.user;
-          await setDoc(doc(usersRef, user.uid), {
-            userId: user.uid,
-            firstName: firstName,
-            lastName: lastName,
-            phoneNumber: phoneNumber,
-            gender: gender,
-            email: email,
-            password: password
-          })
-        }
-      );
+  const signup = (
+    firstName,
+    lastName,
+    phoneNumber,
+    gender,
+    email,
+    password
+  ) => {
+    createUserWithEmailAndPassword(auth, email, password).then(
+      async (userCredential) => {
+        const user = userCredential.user;
+        await setDoc(doc(usersRef, user.uid), {
+          userId: user.uid,
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          gender: gender,
+          email: email,
+          password: password,
+        });
+      }
+    );
   };
   // Login function
   const login = (email, password) => {
@@ -117,13 +127,44 @@ const AuthProvider = ({ children }) => {
   const logout = () => {
     return signOut(auth);
   };
-  const values = { signup, login, logout, currentUser, users, currentUserData, getImage, userImage };
+  let gUser;
+  const googleSignIn = () => {
+    signInWithPopup(auth, gProvider)
+      .then((result) => {
+        const user = result.user;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const facebookSignIn = () => {
+    signInWithPopup(auth, fProvider)
+      .then((result) => {
+        const user = result.user;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const values = {
+    signup,
+    login,
+    logout,
+    facebookSignIn,
+    getImage,
+    googleSignIn,
+    currentUser,
+    users,
+    currentUserData,
+    userImage,
+    gUser,
+  };
 
-  return <AuthContext.Provider value={ values }>{ children }</AuthContext.Provider>;
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
 
 export const useAuth = () => {
   return useContext(AuthContext);
-}
+};
