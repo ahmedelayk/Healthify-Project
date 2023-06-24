c2c3b1c2a7ed47c7597f1e7d3244372b57e100ac
Sameh Hammad <eng.sameh1995@gmail.com>
2023-06-25 02:16:41 +0300

    update contact us section and subscribtion sections

--- src/Components/Home/Sections/section6/Section6.jsx
+++ src/Components/Home/Sections/section6/Section6.jsx
@@ -1,46 +1,127 @@
 // Components
 import { Container, Row } from "react-bootstrap";
 // style
-import './Section6.css';
+import "./Section6.css";
 // Context
 import { useAuth } from "../../../../Context/AuthContext";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import Swal from "sweetalert2";
+
 export default function Section6() {
-  const {t} = useAuth()
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [massege, setMassege] = useState("");
+
+  const handleValue = (e) => {
+    e.preventDefault();
+    if (email.includes("@") && name) {
+      emailjs
+        .sendForm(
+          "service_wz4b2xg",
+          "template_hajy9xu",
+          form.current,
+          "rrNvraXvPIQepy7tI"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      Swal.fire(
+        `${t("Thank You")} <b style=color:#0066ff;>  ${name.toUpperCase()} 
+         </b> ${t("We recieved Your Message Successfully")}`,
+        " <b style=color:#0066ff;> " + `${t("Healthify Team")}` + " </b>",
+        "success"
+      );
+      clearInputs();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: t("Oops!"),
+        text: t("Plz Enter Your Name And Email Correctly"),
+        footer: "",
+      });
+    }
+  };
+  const clearInputs = () => {
+    setName("");
+    setEmail("");
+    setMassege("");
+  };
+  const { t } = useAuth();
   return (
     <>
-    <div id="contact-us" className="container-fluid bg-white section-padding">
-      <Container>
-        <Row>
-          <h1 className="text-center text-paragraph-color">{t("Contact Us")}</h1>
-        </Row>
-        <div className="my-5">
-            <div className='form-container'>
-                <form className="contact-us-form" action="POST">
-                    <div className="form-field-container">
-                        <label className="label-field" htmlFor="name">{t("Name")}</label>
-                        <input className="form-control" type="text" name="name" id="name" />
-                    </div>
-                    <div className="form-field-container">
-                        <label className="label-field" htmlFor="email">{t("E-mail")}</label>
-                        <input className="form-control" type="email" name="email" id="email" />
-                    </div>
-                    <div className="form-field-container">
-                        <label className="label-field" htmlFor="mobile">{t("Mobile")}</label>
-                        <input className="form-control" type="tel" name="mobile" id="mobile" />
-                    </div>
-                    <div className="form-field-container">
-                        <label className="label-area-field" htmlFor="message">{t("Message")}</label>
-                        <textarea className="text-area-field form-control" name="message" id="message" cols="30" rows="5"></textarea>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <input className="submit-btn" type="submit" value={t("Send")} />
-                    </div>
-                    
-                </form>
+      <div id="contact-us" className="container-fluid bg-white section-padding">
+        <Container>
+          <Row>
+            <h1 className="text-center text-paragraph-color">
+              {t("Contact Us")}
+            </h1>
+          </Row>
+          <div className="my-5">
+            <div className="form-container">
+              <form
+                className="contact-us-form"
+                ref={form}
+                onSubmit={handleValue}
+              >
+                <div className="form-field-container">
+                  <label className="label-field" htmlFor="name">
+                    {t("Name")}
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    name="user_name"
+                    id="name"
+                  />
+                </div>
+                <div className="form-field-container">
+                  <label className="label-field" htmlFor="email">
+                    {t("E-mail")}
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    name="user_email"
+                    id="email"
+                  />
+                </div>
+                <div className="form-field-container">
+                  <label className="label-area-field" htmlFor="message">
+                    {t("Message")}
+                  </label>
+                  <textarea
+                    className="text-area-field form-control"
+                    onChange={(e) => setMassege(e.target.value)}
+                    value={massege}
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="5"
+                  ></textarea>
+                </div>
+                <div className="d-flex justify-content-center">
+                  <input
+                    className="submit-btn"
+                    type="submit"
+                    value={t("Send")}
+                  />
+                </div>
+              </form>
             </div>
-        </div>
-      </Container>
-    </div>
+          </div>
+        </Container>
+      </div>
     </>
-  )
+  );
 }
--- src/Components/Workouts/CommonComponents/Subscription.jsx
+++ src/Components/Workouts/CommonComponents/Subscription.jsx
@@ -5,24 +5,87 @@ import "../workout.css";
 import { Button, Form, InputGroup } from "react-bootstrap";
 // Context
 import { useAuth } from "../../../Context/AuthContext";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const Subscription = () => {
+  const [email, setEmail] = useState("");
+
+  const handleValue = async (e) => {
+    e.preventDefault();
+    if (email.includes("@")) {
+      try {
+        const response = await axios.get("http://localhost:3003/subscribers");
+        const subscribers = response.data;
+
+        if (subscribers.some((subscriber) => subscriber.email === email)) {
+          Swal.fire({
+            icon: "info",
+            title: t("Oops!"),
+            text: t("You already subscribed before."),
+            footer: "",
+          });
+        } else {
+          await axios.post("http://localhost:3003/subscribers", { email });
+          Swal.fire(
+            `${t("You have successfully subscribed")}`,
+            ` <b style=color:#0066ff;> ${t("Healthify Team")} </b>`,
+            "success"
+          );
+        }
+        
+        clearInputs();
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: t("Oops!"),
+          text: t("An error occurred. Please try again later."),
+          footer: "",
+        });
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: t("Oops!"),
+        text: t("Please Enter Your Email Correctly"),
+        footer: "",
+      });
+    }
+  };
+
+  const clearInputs = () => {
+    setEmail("");
+  };
+
   const { t } = useAuth();
   return (
     <div className="p-5 newsletter-subscription" data-aos="zoom-in">
-      <h1 className="header1-size text-center">{t("Newsletter Subscription")}</h1>
+      <h1 className="header1-size text-center">
+        {t("Newsletter Subscription")}
+      </h1>
       <div className="p-3" data-aos="zoom-out">
-        <form className="d-flex flex-column flex-lg-row w-75 gap-3 justify-content-center m-auto" action="POST">
-          {/* <InputGroup className="mb-2 mb-lg-0">
-            <Form.Control placeholder="Enter Name" />
-          </InputGroup> */}
+        <form
+          className="d-flex flex-column flex-lg-row w-75 gap-3 justify-content-center m-auto"
+          action="POST"
+        >
           <InputGroup className="mb-2 mb-lg-0">
-            <Form.Control placeholder="Enter Email for subscription" />
+            <Form.Control
+              placeholder="Enter Email for subscription"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              name="user_email"
+            />
           </InputGroup>
-          <Button variant="primary">{t("Subscribe")}</Button>
+          <Button variant="primary" onClick={handleValue}>
+            {t("Subscribe")}
+          </Button>
         </form>
-        <p className="text-center mt-3 mb-0 paragraph-size" data-aos="zoom-in-left">
-          {t("Get to know the latest new and offers we provide on this site.")}
+        <p
+          className="text-center mt-3 mb-0 paragraph-size"
+          data-aos="zoom-in-left"
+        >
+          {t("Get to know the latest news and offers we provide on this site.")}
         </p>
       </div>
     </div>
